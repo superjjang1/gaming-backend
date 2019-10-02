b553134b11a3c2caf66876dd00c73f73e0046470
Sean Yim <superjjang89@gmail.com>
2019-10-02 14:49:18 -0400

    connection between front and back end

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,100 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db')
+const bcrypt = require('bcryptjs');
+const randToken = require('rand-token');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/Register', (req,res,next) => {
+  const {
+    first,
+    last,
+    pass,
+    email
+  } = req.body;
+  if ((!first)||(!last)||(!pass)||(!email)) {
+    res.json({
+      msg: 'not working'
+    });
+    return;
+  }
+  const checkUserQuery = `SELECT * FROM users WHERE email =?`
+  db.query(checkUserQuery, [email], (err,results)=>{
+    if(err) {
+      throw err
+    };
+    if (results.length >0) {
+      res.json({
+        msg: "userExists"
+      })
+    }else {
+      const insertUserQuery = `INSERT INTO users(first, last, email, pass, token) VALUES (?,?,?,?,?)`
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(pass, salt);
+      const token = randToken.uid(50);
+      db.query(insertUserQuery, [first, last, email, hash, token], (err2)=>{
+        if(err2){
+          throw err2
+        }
+        res.json({
+          msg: "userAdded",
+          token,
+          email,
+          first
+        })
+      })
+    }
+  })
+})
+
+router.post('/login', (req, res) => {
+  console.log(req.body)
+  const email = req.body.email;
+  const pass = req.body.password;
+  //first check for this email.
+  const checkLogQuery = `Select * FROM users WHERE email = ?`
+  db.query(checkLogQuery, [email], (err, results) => {
+    if (err) {
+      throw err
+      //check to see if thre's a result
+    };
+    if (results.length > 0) {
+      //at this stage, you've found the user.
+      const thisRow = results[0]
+      //find out if the pass is corrct
+      const isValidPass = bcrypt.compareSync(pass, thisRow.pass)
+      if (isValidPass) {
+        //these are the things we're looking for
+        const token = randToken.uid(50)
+        const updateUserTokenQuery = `UPDATE users SET token = ? WHERE email = ?`
+        db.query(updateUserTokenQuery, [token, email], (err) => {
+          if (err) {
+            throw err
+          }
+        })
+        res.json({
+          msg: "loggedIn",
+          first: thisRow.first,
+          email: thisRow.email,
+          token
+        })
+      } else {
+        //no pass
+        res.json({
+          msg: "badpass"
+        })
+      }
+    } else {
+      //no match
+      res.json({
+        msg: "noEmail"
+      })
+    }
+  })
+})
+
 
 module.exports = router;
