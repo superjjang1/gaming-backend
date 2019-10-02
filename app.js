317f443dd5bbb9f8dc0333dba72cf9973460e500
Sean Yim <superjjang89@gmail.com>
2019-10-02 09:38:42 -0400

    ignore, change port for testing

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//allow cross-origin
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods","GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Conent-Type, Accept, Authorization");
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
