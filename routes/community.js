6a8939abb784638402db413d2ce96f389ec68c82
Sean Yim <superjjang89@gmail.com>
2019-10-11 16:29:33 -0400

    added relation to the community and users on create

--- routes/community.js
+++ routes/community.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router= express.Router();
 const db = require('../db');
 
-router.post('/new',(req,res)=>{
+router.post('/new',async (req,res,next)=>{
     if(!res.locals.loggedIn){
         res.json({
             msg: "badToken"
@@ -14,16 +14,37 @@ router.post('/new',(req,res)=>{
         type,
         description
     } = req.body
+    if ((!name)||(!type)||(!description)) {
+        res.json({
+            msg:'not working'
+        });
+        return;
+    }
     const insertCommunityQuery = `INSERT INTO community (uid, name, type, description) VALUES (?,?,?,?)`;
     const dbValues = [res.locals.uid, name, type, description]
-    let theQuery = db.query(insertCommunityQuery, dbValues,(err)=>{
+
+    let theQuery = await db.query(insertCommunityQuery, dbValues,(err,results)=>{
         if(err) throw err;
-        res.json({
-            msg:"communityAdded"
+        const {
+            UID,
+            CID
+        } = req.body
+        console.log("testing")
+        console.log(results.insertId)
+        const insertCommunityUsers = `INSERT INTO communityUsers (UID, CID) VALUES (?,?)`
+        const dbValues = [res.locals.uid, results.insertId]
+        let newQuery = db.query(insertCommunityUsers, dbValues,(err)=>{
+            if(err) throw err;
+            res.json({
+                msg:"communitypeople added"
+            })
+            console.log(newQuery.sql);
+            console.log('newQuery?');
         })
     })
     console.log(theQuery.sql);
     console.log('doing things')
 })
+
 
 module.exports = router;
